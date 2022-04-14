a03f78770dd3913b0fd818e42b6773d44d6f43f6
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2022-04-14 10:58:13 -0600

    Loops and FPS Mechanics

---
+++ Retro FPS Clone/Assets/Scripts/Assignments/Loops.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+
+    // public int numEwoks = 0;
+
+    // public int ducksInPond = 0; // Wild Loop
+
+    // public int numRainbow = 0;
+
+    // public int starsInSky = 0; // Wild Loop
+
+    // public int numLightsabers = 0;
+
+    // public int snowFlakes = 0; // Wild Loop 
+
+    // public int numMeritBadges = 0;
+
+    // public int Books = 0; // Wild Loop
+
+    // public int numVideoGames = 0;
+    
+    //  public int planetsInSpace = 0; // Wild Loop
+
+
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        for(int i = 99; i >= numEwoks; i--)
+        {
+            Debug.Log("There are " + i + " Ewoks on a log!");
+        }
+        */
+
+        /*
+        while(ducksInPond < 99)
+        {
+            Debug.Log(ducksInPond);
+            Debug.Log("All Ducks have flown south for the winter");
+            ducksInPond ++;
+        }
+        */
+
+        /*
+         for(int i = 99; i >= numrainbow; i--)
+        {
+            Debug.Log("There are " + i + "Rainbows in the sky");
+        }
+        */
+
+        /*
+        while(starsInSky < 99)
+        {
+            Debug.Log(starsInSky);
+            Debug.Log("All the stars in the sky are incredable");
+            starsInSky ++;
+        }
+        */
+
+        /*
+         for(int i = 99; i >= numLightsabers; i--)
+        {
+            Debug.Log("There are " + i + "Lightsabers in the galaxy");
+        }
+        */
+
+        /*
+         while(snowFlakes < 99)
+        {
+            Debug.Log(snowFlakes);
+            Debug.Log("These snowflakes are on of a kind");
+            snowFlakes ++;
+        }
+        */
+
+        /*
+         for(int i = 99; i >= numMeritBadges; i--)
+        {
+            Debug.Log("There are " + i + " meritbadges... To many ");
+        }
+        */
+
+        /*
+         while(Books < 99)
+        {
+            Debug.Log(Books);
+            Debug.Log("All the books have been read over the summer");
+            Books ++;
+        }
+        */
+        
+        /*
+         for(int i = 99; i >= numVideoGames; i--)
+        {
+            Debug.Log("There are " + i + "Video Games on the shelf");
+        }
+        */
+
+        /*
+         while(planetsInSpace < 99)
+        {
+            Debug.Log(planetsInSpace);
+            Debug.Log("All the planets have not been discovered yet");
+            planetsInSpace ++;
+        }
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
