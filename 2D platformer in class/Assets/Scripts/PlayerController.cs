4d84e51a017899ac11304ce71420a37cb0af9b74
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2022-04-18 17:46:06 -0600

    Created 2D Platformer Project (In Class)

---
+++ 2D platformer in class/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Header("PlayerStats")]
+    public float speed;
+    public float jumpHeight;
+    private Rigidbody2D rb;
+
+    [Header("GroundCheck")]
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private float moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // Ground Check sensor
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        // Non-Stick player
+        moveVelocity = 0f;
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+    // Move Player left and right 
+        rb.velocity = new Vector2(moveVelocity,rb.velocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+}
